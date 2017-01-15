e5d3d788081f5e68a8679a7517464639f9d27bba
Wouter Janssens <wouter@makersland.be>
2017-01-15 13:52:21 +0000

    Initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+""" Importeer de modules die je wilt gebruiken """
+import RPi.GPIO as GPIO
+from time import sleep
+
+""" Kies de manier van nummering in je script """
+GPIO.setmode(GPIO.BCM)
+
+""" Configureer pin 2 om gebruikt te worden als output """
+GPIO.setup(2, GPIO.OUT)
+
+""" Starten van een oneindige lus """
+while True:
+    """ We activeren GPIO pin 2 en wachten voor 1 seconde """
+    GPIO.output(2, GPIO.HIGH)
+    sleep(1)
+
+    """ We desactiveren GPIO pin 2 en wachten voor 1 seconde """
+    GPIO.output(2, GPIO.LOW)
+    sleep(1)
