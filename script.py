589a832610799bdcc28b0059097987029c24064d
Wouter Janssens <wouter@makersland.be>
2017-01-19 18:16:27 +0000

    Changed mode to BOARD

--- script.py
+++ script.py
@@ -5,15 +5,15 @@ from time import sleep
 """ Kies de manier van nummering in je script """
 GPIO.setmode(GPIO.BOARD)
 
-""" Configureer pin 2 om gebruikt te worden als output """
+""" Configureer pin 3 om gebruikt te worden als output """
 GPIO.setup(3, GPIO.OUT)
 
 """ Starten van een oneindige lus """
 while True:
-    """ We activeren GPIO pin 2 en wachten voor 1 seconde """
+    """ We activeren GPIO pin 3 en wachten voor 1 seconde """
     GPIO.output(3, GPIO.HIGH)
     sleep(1)
 
-    """ We desactiveren GPIO pin 2 en wachten voor 1 seconde """
+    """ We desactiveren GPIO pin 3 en wachten voor 1 seconde """
     GPIO.output(3, GPIO.LOW)
     sleep(1)
